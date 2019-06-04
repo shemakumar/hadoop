c9e798f6712e43168d25f671d4bed188b047a541
AmithshaS <amithsha.s@flipkart.com>
2019-06-04 09:42:01 +0530

    Added script to buuild cmale install dependencies and protobuff

---
+++ buildCmake.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+tar -xvf cmake-3.13.3-Linux-x86_64.tar.gz 
+cd cmake-3.13.3-Linux-x86_64/
+sudo ln -s bin/cmake /usr/bin/cmake
+export PATH=$cwd"/cmake-3.13.3-Linux-x86_64/bin:$PATH"
+echo $PATH
+cmake --version
+#export PATH=$cwd"/cmake-3.13.3-Linux-x86_64/bin:$PATH"
+#echo $PATH
---
+++ buildProtobuff.sh
@@ -0,0 +1,7 @@
+cd protobuf-2.5.0
+sudo ./configure
+sudo make clean
+sudo make
+sudo make install
+sudo ldconfig
+#protoc --version
