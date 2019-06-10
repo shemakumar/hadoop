deda999aabc277c4c3b57ff12462fae8c694faaf
AmithshaS <amithsha.s@flipkart.com>
2019-06-10 12:50:23 +0530

    build script change

--- buildScripts/pushTarToRepo.sh
+++ buildScripts/pushTarToRepo.sh
@@ -14,5 +14,5 @@ cp -rf DEBIAN deb/
 
 #sh make-flipkart-yarn-2.9.0-deb 
 dpkg -b deb flipkart-hadoop-tar-test.deb
-#reposervice --host repo-svc-app-0001.nm.flipkart.com --port "8080" pubrepo --repo  fk-hadoop-tar-test-source  --appkey dummy --debs flipkart-hadoop-tar-test.deb
+reposervice --host repo-svc-app-0001.nm.flipkart.com --port "8080" pubrepo --repo  fk-hadoop-tar-test-source  --appkey dummy --debs flipkart-hadoop-tar-test.deb
 cd $cwd
