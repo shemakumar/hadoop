2f3c9adb041f1d373627f602de21adcacfe0da38
Hema Kumar S <hema.kumar@flipkart.com>
2021-06-16 10:29:24 +0530

    adding hadoop test repo creation script

---
+++ buildScripts/pushTarToTestRepo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir pushHadoopTar/HadoopSourceBinary
+cp -rf hadoop-dist/target/hadoop-3.2.1.tar.gz pushHadoopTar/HadoopSourceBinary/
+ls
+
+cd pushHadoopTar
+ls
+
+mkdir -p HadoopSourceBinary
+mkdir -p deb/tmp/
+cp -rf HadoopSourceBinary deb/tmp/
+cp -rf DEBIAN deb/
+
+sh pushHadoopTar/make-flipkart-hadoop-source-tar-3.2.1-deb
+dpkg -b deb flipkart-hadoop-source-tar-repo-test.deb
+reposervice --host 10.24.0.41 --port "8080" pubrepo --repo  fk-hadoop-source-tar-repo-test  --appkey dummy --debs flipkart-hadoop-source-tar-repo-test.deb
+cd $cwd
